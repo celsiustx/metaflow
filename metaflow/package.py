3782f821fc9719e5ff5f17f788dfe0d1188a6e65
Ryan Williams <ryan@runsascoded.com>
2021-08-09 17:55:20 -0400

    use correct flow file / entrypoint in --batch mode

--- metaflow/package.py
+++ metaflow/package.py
@@ -1,4 +1,5 @@
 import os
+from os import getcwd
 import sys
 import tarfile
 import time
@@ -51,11 +52,18 @@ class MetaflowPackage(object):
                 deco.package_init(flow, step.__name__, environment)
         self.blob = self._make()
 
-    def _walk(self, root, exclude_hidden=True, addl_suffixes=None):
+    def _walk(
+        self, root, exclude_hidden=True, addl_suffixes=None, parent_dir_namespace=True
+    ):
         if addl_suffixes is None:
             addl_suffixes = []
         root = to_unicode(root)  # handle files/folder with non ascii chars
-        prefixlen = len("%s/" % os.path.dirname(root))
+        if parent_dir_namespace:
+            prefixlen = len("%s/" % os.path.dirname(root))
+        else:
+            if not root.endswith("/"):
+                root += "/"
+            prefixlen = len(root)
         for path, dirs, files in os.walk(root):
             if exclude_hidden and "/." in path:
                 continue
@@ -117,8 +125,8 @@ class MetaflowPackage(object):
                 yield path_tuple
         else:
             # the user's working directory
-            flowdir = os.path.dirname(os.path.abspath(sys.argv[0])) + "/"
-            for path_tuple in self._walk(flowdir):
+            cwd = getcwd()
+            for path_tuple in self._walk(cwd, parent_dir_namespace=False):
                 yield path_tuple
 
     def _add_info(self, tar):
